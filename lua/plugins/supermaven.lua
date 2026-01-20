27e19e04988866da5101b4b1f8b1d83c62b7484d
Julius Breindl <jbr@condat.de>
2026-01-20 11:40:05 +0100

    prevent autocomplete from org files

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -3,15 +3,16 @@ return {
         "supermaven-inc/supermaven-nvim",
         config = function()
             require("supermaven-nvim").setup({
-                keymaps = {
+                keymaps                   = {
                     accept_suggestion = "<C-l>",
                     clear_suggestion = "<C-]>",
                     accept_word = "<C-j>",
                 },
-                log_level = "info",                -- set to "off" to disable logging completely
+                log_level                 = "info", -- set to "off" to disable logging completely
                 disable_inline_completion = false, -- disables inline completion for use with cmp
-                disable_keymaps = false,           -- disables built in keymaps for more manual control
-                condition = function()
+                disable_keymaps           = false, -- disables built in keymaps for more manual control
+                ignore_filetypes          = { "norg" },
+                condition                 = function()
                     return false
                 end -- condition to check for stopping supermaven, `true` means to stop supermaven
             })
